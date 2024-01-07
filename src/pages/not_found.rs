f012c6999a836ee7f6991e7573ce00357863ba75
Erik Kurjak <e.kurjak@gmail.com>
2024-01-07 15:31:04 +0100

    feat: update not found page

--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -1,5 +1,7 @@
 use leptos::*;
 
+use crate::components::{header::Header, ui::layout::Layout};
+
 #[component]
 pub fn NotFound() -> impl IntoView {
     #[cfg(feature = "ssr")]
@@ -9,23 +11,29 @@ pub fn NotFound() -> impl IntoView {
     }
 
     view! {
-        <div class="flex min-h-full flex-col">
-            <main class="mx-auto flex w-full max-w-7xl flex-auto flex-col justify-center px-6 py-24 sm:py-64 lg:px-8">
-                <p class="text-base font-semibold leading-8 text-indigo-600">404</p>
-                <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-                    Page not found
+        <Header/>
+        <main class="grid gap-28 lg:gap-64 mt-10 md:mt-28">
+            <h2 class="text-[520px] text-gray-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-medium">
+                404
+            </h2>
+            <Layout aria_label="Not Found" class_name="flex-col">
+                <h1 class="text-5xl xs:text-6xl sm:text-7xl lg:text-8xl xl:text-10xl tracking-tight text-gray-9 lg:leading-tighter">
+                    Page <span class="font-light text-gray-9">not</span> <br/> not found
                 </h1>
-                <p class="mt-6 text-base leading-7 text-gray-600">
-                    "Sorry, we couldn’t find the page you’re looking for."
+                <p class="text-xl md:text-2xl lg:text-3xl lg:leading-relaxed leading-relaxed text-gray-9 mt-10 lg:mt-20">
+                    Sorry, we "couldn’t" find the <b>
+                        <i>page</i>
+                    </b> " you’re" looking for
                 </p>
-                <div class="mt-10">
-                    <a href="#" class="text-sm font-semibold leading-7 text-indigo-600">
-                        <span aria-hidden="true">&larr;</span>
-                        Back to home
-                    </a>
-                </div>
-            </main>
-        </div>
+                <a
+                    class="text-md md:text-lg lg:text-xl lg:leading-relaxed leading-relaxed text-gray-9 mt-2 lg:mt-4 font-bold"
+                    href="/"
+                >
+                    "Let’s"
+                    go home
+                </a>
+            </Layout>
+        </main>
     }
 }
 
