44a6d905fdaad9d76c7c2eb00483d3cbfe51d943
Erik Kurjak <e.kurjak@gmail.com>
2024-02-28 16:31:06 +0100

    fix: hide image fixed circle on table and mobile

--- src/components/about.rs
+++ src/components/about.rs
@@ -23,7 +23,7 @@ pub fn ScrollWatchImage() -> impl IntoView {
 #[component]
 pub fn About() -> impl IntoView {
     view! {
-        <div class="w-full h-full lg:w-56 lg:h-56 flex justify-center items-center fixed right-10 bottom-10 z-10">
+        <div class=" w-full h-full lg:w-56 lg:h-56 hidden lg:flex justify-center items-center fixed right-10 bottom-10 z-10">
             <div class="w-48 h-48 rounded-full bg-ek-orange flex justify-center items-center">
                 <picture class="absolute flex items-center">
                     <source
